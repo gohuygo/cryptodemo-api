aff245b5babff8ae2a0c085080a009c218aedf77
Huy <huy@gohuygo.com>
2018-08-08 13:42:55 -0700

    Print out ETH price

--- main.go
+++ main.go
@@ -1,21 +1,41 @@
 package main
 
 import(
+  cmc "github.com/coincircle/go-coinmarketcap"
   "fmt"
   "net/http"
   "os"
+  "time"
+  "strconv"
 )
 
 func main() {
   port := os.Getenv("PORT") 
+
   if port  == "" {
     port = "8080"
   }
+  
   http.HandleFunc("/", index)
   
   http.ListenAndServe(":" + port, nil)
 }
 
 func index(w http.ResponseWriter, r *http.Request) {
-  fmt.Fprintf(w, "Hello World")
+  threeMonths := int64(60 * 60 * 24 * 90)
+  now := time.Now()
+  secs := now.Unix()
+  start := secs - threeMonths
+  end := secs
+
+  fmt.Println("Time is " + strconv.FormatInt(end, 10))
+  graph, _ := cmc.TickerGraph(&cmc.TickerGraphOptions{
+    Start: start,
+    End: end,
+    Symbol: "ETH",
+  })
+
+  // fmt.Println(graph.PriceBTC)
+  fmt.Fprintln(w, graph.PriceBTC)
+
 }
