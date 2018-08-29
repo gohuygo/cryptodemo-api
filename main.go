fee289238994f2b0019f01c1bcacb75a2ee0862e
Huy <huy@gohuygo.com>
2018-08-29 12:08:52 -0700

    Add protected endpoint logic

--- main.go
+++ main.go
@@ -14,7 +14,7 @@ import(
   "github.com/dgrijalva/jwt-go"
   // "github.com/gorilla/context"
   "github.com/gorilla/mux"
-  // "github.com/mitchellh/mapstructure"
+  "github.com/mitchellh/mapstructure"
 )
 
 type User struct {
@@ -26,15 +26,22 @@ type JwtToken struct {
   Token string `json:"token"`
 }
 
+type Exception struct {
+  Message string `json:"message"`
+}
+
 func CreateTokenEndpoint(w http.ResponseWriter, req *http.Request) {
   fmt.Println("Create Token Endpoint")
 
   var user User
-  _ = json.NewDecoder(req.Body).Decode(&user)
 
+  _ = json.NewDecoder(req.Body).Decode(&user)
+  fmt.Println(req.Body)
+  fmt.Println(user)
   token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
-    "name":     user.Name,
+    "name":  user.Name,
   })
+
 
   tokenString, err := token.SignedString([]byte("testsecret"))
   if err != nil { fmt.Println(err) }
@@ -43,13 +50,32 @@ func CreateTokenEndpoint(w http.ResponseWriter, req *http.Request) {
 }
 
 func ProtectedEndpoint(w http.ResponseWriter, req *http.Request) {
-  fmt.Println("Protected Endpoint")
+  fmt.Println("Protected Endpoint!!")
+  params := req.URL.Query()
+
+  token, _ := jwt.Parse(params["token"][0], func(token *jwt.Token) (interface{}, error) {
+    if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+      return nil, fmt.Errorf("There was an error")
+    }
+    return []byte("testsecret"), nil
+  })
+
+  fmt.Println(params)
+
+  if claims, ok := token.Claims.(jwt.MapClaims); ok && token.Valid {
+      var user User
+      mapstructure.Decode(claims, &user)
+      json.NewEncoder(w).Encode(user)
+  } else {
+      json.NewEncoder(w).Encode(Exception{Message: "Invalid authorization token"})
+  }
 }
 
 
 func main() {
-  // Set production port
+  // Set production port (Heroku)
   port := os.Getenv("PORT")
+
   // Set development port
   if port  == "" {
     port = "8080"
@@ -61,6 +87,7 @@ func main() {
   router.HandleFunc("/authenticate",  CreateTokenEndpoint).Methods("POST")
   router.HandleFunc("/protected",  ProtectedEndpoint).Methods("GET")
   router.HandleFunc("/", IndexEndpoint)
+
   log.Fatal(http.ListenAndServe(":"+port, router))
 }
 
